edb267854e8c34bb324a38362cbffad311202464
Keanu Clark <35940194+kaclark@users.noreply.github.com>
2020-08-07 18:26:36 -0500

    fixed scde

--- ensembl_symbol_conversion.R
+++ ensembl_symbol_conversion.R
@@ -3,12 +3,13 @@ require(jsonlite)
 
 url = "https://biotools.fr/mouse/ensembl_symbol_converter/"
 raw <- scan("./data/rna/ensembl.txt", character())
+print(raw)
 ids_json <- toJSON(raw)
 print("Ids loaded")
 body <- list(api=1, ids=ids_json)
 r <- POST(url, body = body)
 
-output = fromJSON( content(r, "text"), flatten=FALSE)
+output = fromJSON( content(r, "text"), flatten=TRUE)
 df <- as.data.frame(output)
 print(df)
 write.table(df,file="./data/rna/symbols.txt",row.names=TRUE)
--- scde.R
+++ scde.R
@@ -1,10 +1,16 @@
 require(scde)
-rna_data = read.table('./data/rna/qc_counts.txt', sep='\t')
-rna_data<-apply(rna_data,2,function(x) {storage.mode(x) <- 'integer'; x})
-err <- scde.error.models(counts = rna_data, n.cores = 1, min.size.entries = 2000, threshold.segmentation = TRUE, save.crossfit.plots = FALSE, save.model.plots = FALSE, verbose = 1)
+#load rna raw count data
+rna_data = read.csv('./data/rna/qc_counts.txt', row.names=1, header=TRUE, sep='\t')
+#clean data/filter data
+rna_data <- clean.counts(rna_data, min.lib.size=1000, min.reads = 1, min.detected=1)
+#generate error model
+err <- scde.error.models(counts = rna_data, n.cores = 4, threshold.segmentation = TRUE, save.crossfit.plots = FALSE, save.model.plots = FALSE, verbose = 1)
+#filter poor fits
 valid.cells <- err$corr.a > 0
 err <- err[valid.cells,]
-o.prior <- scde.expression.prior(models = err, counts = data, length.out = 400, show.plot = False)
-ediff <- scde.expression.difference(err, cd, o.prior, n.randomizations = 100, n.cores = 1, verbose = 1)
+o.prior <- scde.expression.prior(models = err, counts = rna_data, length.out = 400, show.plot = False)
+#get differential data
+ediff <- scde.expression.difference(err, rna_data, o.prior, n.randomizations = 100, n.cores = 4, verbose = 1)
 head(ediff[order(ediff$Z, decreasing = TRUE), ])
+#export data
 write.table(ediff[order(abs(ediff$Z), decreasing = TRUE), ], file = "./data/rna/scde_results.txt", row.names=TRUE, col.names=TRUE, sep="\t", quote = FALSE)
