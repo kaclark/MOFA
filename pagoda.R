8262093a5e87dd1373fcbc34a19a5bc0aed3fb97
Keanu Clark <35940194+kaclark@users.noreply.github.com>
2020-08-09 08:24:04 -0500

    changed write.csv to saveRDS because the dataset is odd and refused to be saved to a txt file

--- pagoda.R
+++ pagoda.R
@@ -3,7 +3,7 @@ require(scde)
 rna_data = read.csv('./data/rna/qc_counts.txt', row.names=1, header=TRUE, sep='\t')
 cd <- clean.counts(rna_data)
 #clean data/filter data
-knn <- knn.error.models(cd, k = ncol(cd)/12, n.cores = detectCores(), min.count.threshold = 10, min.nonfailed = 10, max.model.plots = 10)
-varinfo <- pagoda.varnorm(knn, counts = cd, trim = 10/ncol(cd), max.adj.var = 5, n.cores = detectCores(), plot = TRUE)
+knn <- knn.error.models(cd, k = ncol(cd)/12, n.cores = 8, min.count.threshold = 10, min.nonfailed = 10, max.model.plots = 10)
+varinfo <- pagoda.varnorm(knn, counts = cd, trim = 10/ncol(cd), max.adj.var = 5, n.cores = 8, plot = TRUE)
 sort(varinfo$arv, decreasing = TRUE)[1:10]
-write.table(varinfo, file = "./data/rna/pagoda_results.txt", row.names=TRUE, col.names=TRUE, sep="\t", quote = FALSE)
+saveRDS(varinfo, "./data/rna/pagoda_results.rds")
